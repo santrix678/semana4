536deef1e07cfecf6385fa671ff0b9a1a4f326fb
santrix678 <sj.riosm@uea.edu.ec>
2026-01-11 04:09:06 +0100

    semana5

---
+++ semana 5/script.js
@@ -0,0 +1,7 @@
+function cambiaTexto() {
+    document.getElementById("texto").innerHTML = "¡Has cambiado el texto!";
+}
+
+function cambiaColor() {
+    document.getElementById("color").style.backgroundColor = "lightblue";
+}
